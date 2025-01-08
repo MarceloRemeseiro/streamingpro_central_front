a6ca39ff34fa439c188bd5666ed56a81fe41379d
marceloremeseiro <marcelo.remeseiro@gmail.com>
2025-01-08 05:08:37 +0100

    next config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,19 @@ const nextConfig = {
   output: 'standalone',
   reactStrictMode: true,
   swcMinify: true,
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "upgrade-insecure-requests"
+          }
+        ]
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig 
