403011235731ebe502bea71c705af813a4d39e05
marceloremeseiro <marcelo.remeseiro@gmail.com>
2025-01-22 11:50:11 +1100

    eliminar devices

--- src/utils/api.ts
+++
@@ -1,5 +0,0 @@
-export const getRestreamerApiUrl = (): string => {
-  const baseUrl = process.env.NEXT_PUBLIC_RESTREAMER_BASE_URL || 'lvp.streamingpro.es';
-  const port = process.env.NEXT_PUBLIC_RESTREAMER_API_PORT || '8080';
-  return `https://${baseUrl}:${port}/api`;
-}; 
